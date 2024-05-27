4ee504014fe438421233a2e7e48463701b0238a6
jhon <91237764+RuberDucky@users.noreply.github.com>
2024-05-27 16:29:17 +0500

    chore: Update resume PDF filename to v24.4

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,8 +11,6 @@ import vazra from "../../Assets/Projects/vazra.png";
 import fitsRight from "../../Assets/Projects/fitsRight.png";
 
 
-
-
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -33,6 +31,7 @@ function Projects() {
               description="StandMan is a Flutter-based mobile application that acts as a bridge between customers and employees for job creation and fulfillment. It offers features like job acceptance/rejection, preference radius setting, and user communication. It also includes a support contact and job history tracking. The app leverages location services to match jobs with nearby employees. Payments are processed via Stripe. The Laravel PHP backend was developed by a colleague."
               ghLink="https://github.com/RuberDucky/stand_manv2"
               // demoLink="https://chatify-49.web.app/"
+              playStoreLink="https://play.google.com/store/apps/details?id=com.standman.app"
             />
           </Col>
 
@@ -42,67 +41,67 @@ function Projects() {
               isBlog={false}
               title="Passporttastic"
               description="Passporttastic is a unique mobile application that allows users to create digital passports. After a successful sign-up with authentic information, users can select the country they wish to travel to and create a corresponding digital passport. The app offers features such as viewing the passport profile, purchasing passport stamps for travel, and adding pages to the passport. Users can also customize their passport with stamps for the countries they plan to travel to. The primary purpose of Passporttastic is to facilitate the creation and management of digital passports."
-              ghLink="https://github.com/RuberDucky/" // replace with the link to your GitHub repository
-              // demoLink="https://passporttastic-demo-link.com/" // replace with the link to your demo if available
+            //ghLink="https://github.com/RuberDucky/" // replace with the link to your GitHub repository
+            // demoLink="https://passporttastic-demo-link.com/" // replace with the link to your demo if available
             />
           </Col>
 
           <Col md={4} className="project-card">
-  <ProjectCard
-    imgPath={yulo} // replace with the path to your Yulo app image
-    isBlog={false}
-    title="Yulo"
-    description="Yulo is a clone of TikTok that I contributed to. I worked on solving a number of issues and added new features, making them well-optimized. The main problem I solved was an issue with video uploads not working properly. This issue has been resolved and the app is now functioning correctly. The app will soon be uploaded to the Google Play Store and the App Store."
-    ghLink="https://github.com/YourUsername/yulo" // replace with the link to your GitHub repository
-    playStoreLink="https://play.google.com/store/apps/details?id=com.yuloapp.app" // replace with the link to your app on the Play Store when available
-    // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
-  />
-</Col>
-<Col md={4} className="project-card">
-  <ProjectCard
-    imgPath={deliverClient} // replace with the path to your Deliver Client app image
-    isBlog={false}
-    title="Deliver Client"
-    description="Deliver Client is an app that allows users to book rides for the delivery of parcels and couriers. I made significant contributions to this app, including adding new features and solving issues. The app allows users to book a ride, and the available delivery partner can accept the delivery offer and start the ride. Once the parcel is delivered, users can leave reviews and use many other features."
-    // ghLink="https://github.com/YourUsername/deliver-client" // replace with the link to your GitHub repository
-    playStoreLink="https://play.google.com/store/apps/details?id=com.deliverbygfl.client" // replace with the link to your app on the Play Store when available
-    // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
-  />
-</Col>
+            <ProjectCard
+              imgPath={yulo} // replace with the path to your Yulo app image
+              isBlog={false}
+              title="Yulo"
+              description="Yulo is a clone of TikTok that I contributed to. I worked on solving a number of issues and added new features, making them well-optimized. The main problem I solved was an issue with video uploads not working properly. This issue has been resolved and the app is now functioning correctly. The app will soon be uploaded to the Google Play Store and the App Store."
+              // ghLink="https://github.com/YourUsername/yulo" // replace with the link to your GitHub repository
+              playStoreLink="https://play.google.com/store/apps/details?id=com.yuloapp.app" // replace with the link to your app on the Play Store when available
+            // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={deliverClient} // replace with the path to your Deliver Client app image
+              isBlog={false}
+              title="Deliver Client"
+              description="Deliver Client is an app that allows users to book rides for the delivery of parcels and couriers. I made significant contributions to this app, including adding new features and solving issues. The app allows users to book a ride, and the available delivery partner can accept the delivery offer and start the ride. Once the parcel is delivered, users can leave reviews and use many other features."
+              // ghLink="https://github.com/YourUsername/deliver-client" // replace with the link to your GitHub repository
+              playStoreLink="https://play.google.com/store/apps/details?id=com.deliverbygfl.client" // replace with the link to your app on the Play Store when available
+            // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
+            />
+          </Col>
 
-<Col md={4} className="project-card">
-  <ProjectCard
-    imgPath={deliverPartner} // replace with the path to your Deliver Partner app image
-    isBlog={false}
-    title="Deliver Partner"
-    description="Deliver Partner is the companion app to Deliver Client. It allows delivery partners to accept delivery offers and start rides. I also made significant contributions to this app, including adding new features and solving issues. Once the delivery partner has delivered the parcel, they can mark the delivery as completed."
-    ghLink="https://github.com/YourUsername/deliver-partner" // replace with the link to your GitHub repository
-    playStoreLink="https://play.google.com/store/apps/details?id=com.deliverbygfl.partner" // replace with the link to your app on the Play Store when available
-    // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
-  />
-</Col>
-<Col md={4} className="project-card">
-  <ProjectCard
-    imgPath={vazra} // replace with the path to your Vazra app image
-    isBlog={false}
-    title="Vazra"
-    description="Vazra is a clone of WhatsApp that I contributed to. I worked on implementing Firebase Notifications, push notifications for calls and messages, and user blocking functionality. I also worked on reporting messages and overall performance optimization."
-    // ghLink="https://github.com/YourUsername/vazra" // replace with the link to your GitHub repository
-    playStoreLink="https://play.google.com/store/apps/details?id=com.vazra.apps" // replace with the link to your app on the Play Store when available
-    // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
-  />
-</Col>
-<Col md={4} className="project-card">
-  <ProjectCard
-    imgPath={fitsRight} // replace with the path to your FitsRight app image
-    isBlog={false}
-    title="FitsRight"
-    description="FitsRight is a tailor app that I contributed to. Users can input their body size and the app will show branded clothes that fit them. We are currently working on implementing 3D models. Users will be able to upload images and our AI model will measure their size and automatically fit the clothes that are suitable for them."
-  //  ghLink="https://github.com/YourUsername/fitsright" // replace with the link to your GitHub repository
-    // playStoreLink="https://play.google.com/store/apps/details?id=com.yourcompany.fitsright" // replace with the link to your app on the Play Store when available
-    // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
-  />
-</Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={deliverPartner} // replace with the path to your Deliver Partner app image
+              isBlog={false}
+              title="Deliver Partner"
+              description="Deliver Partner is the companion app to Deliver Client. It allows delivery partners to accept delivery offers and start rides. I also made significant contributions to this app, including adding new features and solving issues. Once the delivery partner has delivered the parcel, they can mark the delivery as completed."
+              ghLink="https://github.com/YourUsername/deliver-partner" // replace with the link to your GitHub repository
+              playStoreLink="https://play.google.com/store/apps/details?id=com.deliverbygfl.partner" // replace with the link to your app on the Play Store when available
+            // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={vazra} // replace with the path to your Vazra app image
+              isBlog={false}
+              title="Vazra"
+              description="Vazra is a clone of WhatsApp that I contributed to. I worked on implementing Firebase Notifications, push notifications for calls and messages, and user blocking functionality. I also worked on reporting messages and overall performance optimization."
+              // ghLink="https://github.com/YourUsername/vazra" // replace with the link to your GitHub repository
+              playStoreLink="https://play.google.com/store/apps/details?id=com.vazra.apps" // replace with the link to your app on the Play Store when available
+            // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={fitsRight} // replace with the path to your FitsRight app image
+              isBlog={false}
+              title="FitsRight"
+              description="FitsRight is a tailor app that I contributed to. Users can input their body size and the app will show branded clothes that fit them. We are currently working on implementing 3D models. Users will be able to upload images and our AI model will measure their size and automatically fit the clothes that are suitable for them."
+            //  ghLink="https://github.com/YourUsername/fitsright" // replace with the link to your GitHub repository
+             playStoreLink="https://play.google.com/store/apps/details?id=com.fitsright.app" // replace with the link to your app on the Play Store when available
+            // appStoreLink="https://apps.apple.com/app/idYourAppId" // replace with the link to your app on the App Store when available
+            />
+          </Col>
           {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
